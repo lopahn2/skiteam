bb2a019711285c5ef2a42172ce1302642cb34beb
hwany <lopahn2@gmail.com>
2023-01-11 15:49:15 +0900

    refactor: git ignore 문제 수정 --- 파일 삭제 후 이그노어 파일 수정 후 푸쉬

--- mysql_cmd/user.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `user` (
-  `id` varchar(255) NOT NULL,
-  `pwd` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `salt` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-)
