2db3d814b384b6d649187986a39621377337b92a
hwany <lopahn2@gmail.com>
2023-02-15 14:21:34 +0900

    fetch 23-02-15: DB connection을 single에서 pool로 변경

--- db/sqlCon.js
+++ db/sqlCon.js
@@ -5,13 +5,23 @@ module.exports = async () => {
 	// get the promise implementation, we will use bluebird
 	const bluebird = require('bluebird');
 	// create the connection, specify bluebird as Promise
-	const connection = await mysql.createConnection(
+	// const connection = await mysql.createConnection(
+	// 	{
+	// 		host: process.env.HOST, 
+	// 		user: process.env.DBUSER, 
+	// 		//password: process.env.PASSWORD,
+	// 		database: process.env.DATABASE, 
+	// 		Promise: bluebird
+	// 	});
+
+	// create the connectionPool, specify bluebird as Promise
+	const connection = await mysql.createPool(
 		{
 			host: process.env.HOST, 
 			user: process.env.DBUSER, 
 			//password: process.env.PASSWORD,
 			database: process.env.DATABASE, 
 			Promise: bluebird
-		});
+		});	
 	return connection
 }
